8ab9779e31820fdcbbe885a9e8e15094fca7e3cc
MiquéiasC <miqueiascampos344@gmail.com>
2023-11-21 17:08:51 -0300

    aula 1 introdução
    aula 1 introdução

---
+++ Aula 01- Introdução/materialdeaula/js/script02.js
@@ -0,0 +1,30 @@
+alert("Calculadora de soma!");
+//var numero1 = prompt("Digite o seu primeiro número:");
+//var numero2 = prompt("Digite o seu segundo número:");
+//primeira forma
+//var soma = parseFloat(numero1) + parsefloat(numero2); // parse é responsavel por conversão parseflot (com decimal) parseint(inteiro)
+
+
+//alert(soma);
+
+
+
+//primeira forma
+alert("Calculadora de soma!");
+var numero1 = Number(prompt("Digite o seu primeiro número:"));
+var numero2 = Number(prompt("Digite o seu segundo número:"));
+
+
+
+alert(soma);
+
+
+//OPERADORES ARITMÉTICOS
+// + SOMAR(CONCATENAR)
+// - SUBTRAIR
+// * MULTIPLICAR
+// ** POTÊNCIA
+// / DIVIDIR
+// % RESTO DA DIVISÃO
+// ++ INCREMENTAR (variável = variável + 1 ) varialvel++
+// -- DECRESCENTAR (variável = variável - 1 ) varialvel--
