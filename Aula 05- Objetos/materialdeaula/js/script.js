5e1004e214db04e4766f7290ab63e96c6291cd2c
LETICIA-GB <146875822+LETICIA-GB@users.noreply.github.com>
2023-11-28 17:42:52 -0300

    atividade da aula 5
    atividade da aula 5

--- Aula 05- Objetos/materialdeaula/js/script.js
+++ Aula 05- Objetos/materialdeaula/js/script.js
@@ -15,20 +15,38 @@ class Cliente{
     conta;
     saldo;
     //ação é igual a função
-    //--------------------pegar do git do prof, daqui para baixo------
-    sacar(){
-        //this.atributo: Seleciona um atributo
-        this.saldo = this.saldo - valor;
-    }
-    depositar(){}
+   
+ 
+      
+        verificaSaldo(){
+            return "  O saldo da conta é de R$ "+this.saldo;
+        }
+        sacar(valor){
+            if(this.saldo >= valor){
+                //this.atributo: Seleciona um atributo
+                this.saldo = this.saldo - valor;
+                return "Você sacou : R$ "+valor+this.verificaSaldo();
+                
+            }else{
+                return "Operação não autorizada! Você está quebrado :(";
+            }
+        }
+        depositar(valor){
+            if(valor <= 0){
+                return "Operação não autorizada! Minimo de deposito R$ 0.01";
+            }else{
+                this.saldo = this.saldo + valor;
+                return "Depósito efetuado! R$ "+valor+this.verificaSaldo();
+            }
+        }
 }
-var cliente1 = new Cliente();
-var cliente2 = new Cliente();
-
-cliente1.nome = "Leonardo";
-cliente1.cpf = "999999999";
-cliente1.endereco = "Taguatinga rua E casa 02"
-cliente1.conta = "001"
-cliente1.saldo = "0"
-cliente1.telefone = "61 9999-9999"
-console.log(cliente1)
+    
+    var cliente1 = new Cliente();
+    var cliente2 = new Cliente();
+    cliente1.nome = "Leonardo";
+    cliente1.cpf = 9999999;
+    cliente1.endereco = "Taguatinga bloco E rua 30";
+    cliente1.telefone = 619999-9999;
+    cliente1.conta = "001";
+    cliente1.saldo = 0;
+    console.log(cliente1)
