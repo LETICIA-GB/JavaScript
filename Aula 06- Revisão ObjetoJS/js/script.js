f65cf643432e533248ef2394e9f1589706d53a50
LETICIA-GB <146875822+LETICIA-GB@users.noreply.github.com>
2023-11-29 16:52:30 -0300

    inicio da aula 7 e 8
    inicio da aula 7 e 8

--- Aula 06- Revisão ObjetoJS/js/script.js
+++ Aula 06- Revisão ObjetoJS/js/script.js
@@ -19,26 +19,32 @@ var listabtn = document.querySelector("#listabotao") // do segundo botão- o lis
 cadastrobtn.addEventListener("click", cadastrar)
 listabtn.addEventListener("click", lista) // do segundo botão- o lista
 
-var filme = null
+var filmes = []
 
 function cadastrar(){
-    let nomeinput = document.querySelector("#nomeFilme").value // depois do let eu posso cirar qualquer nome da let, o da# tem q ser a mesma q coloquei no html
+    let nomeinput = document.querySelector("#nomeFilme").value // depois do let eu posso criar qualquer nome da let, o da# tem q ser a mesma q coloquei no html
     let descricaofinput = document.querySelector("#descricaof").value
     let datafinput = document.querySelector("#dataf").value
     let diretorfinput = document.querySelector("#diretorf").value
     let categoriafinput = document.querySelector("#categoriaf").value
 
-    filme = new Filme(nomeinput,descricaofinput,datafinput,diretorfinput,categoriafinput)
+    filmes.push(new Filme(nomeinput,descricaofinput,datafinput,diretorfinput,categoriafinput))
      
 }
 
 function lista(){
     let resposta = document.querySelector("#resposta")
+
+    resposta.innerHTML = ""  // comando para limpar
+
+    filmes.forEach((cadafilme)=> {
     resposta.innerHTML += `<div class="filme" id="respostadalistaFilme">
-            <h2>${filme.nome}</h2>
-            <p> Descrição do filme${filme.descricao}</p>
-            <p> Data do filme${filme.data}</p>
-            <p> Diretor do filme${filme.diretor}</p>
-            <p style="color: blue;"> ${filme.categoria}</p>
+            <h2>${cadafilme.nome}</h2>
+            <p> Descrição do filme${cadafilme.descricao}</p>
+            <p> Data do filme${cadafilme.data}</p>
+            <p> Diretor do filme${cadafilme.diretor}</p>
+            <p style="color: blue;"> ${cadafilme.categoria}</p>
     </div> `
+})
+
 }
