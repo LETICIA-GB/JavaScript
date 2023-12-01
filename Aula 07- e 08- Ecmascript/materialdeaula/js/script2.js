65555a28b4a5f22096d2555da33d0d95a5641238
LETICIA-GB <146875822+LETICIA-GB@users.noreply.github.com>
2023-12-01 15:47:35 -0300

    AULA 7 E 8
    continuação

---
+++ Aula 07- e 08- Ecmascript/materialdeaula/js/script2.js
@@ -0,0 +1,78 @@
+// *PROMISSES
+
+var primeiropromisse = new Promise((resolve, reject) =>{
+    //PROMISSES SE ASESEMELHAM A PROMESSAS, PODEMOS CUMPRI-LAS OU NÃO
+    var nome = "Alice"
+
+    if (nome == "Leticia"){
+        // PROMESSA CUMPRIDA
+        resolve("Seja bem vinda Leticia")
+    }else{
+        //PROMESSA QUEBRADA
+        reject("Usuario não autorizado")
+    }
+})
+
+//then; esper resolve
+//catch: espera o reject
+
+primeiropromisse.then((data) => {
+    console.log(data)
+})
+
+
+//*AWAIT
+function sujeitodemorado(){
+    return new Promise((resolve) =>{
+        setTimeout(()=>{
+            console.log("Tudo bem")
+            resolve()
+        }, 5000)
+})
+
+}
+
+// RODA ASFUNÇÕES DE FORMA ASSÍNCRONA
+async function msg(){
+
+console.log ("boa tarde")
+// ESPERA UM PROCESSO ACONTECER ANTES DE CONTINUAR LENDO O CÓDIGO
+await sujeitodemorado()
+console.log ("Espero um excelente dia")
+
+}
+msg()  //chama a função
+
+
+//*OBJECT ENTRIES
+
+var computador = {
+    sistemaoperacional: "windows",
+    placamae: "Rog strix",
+    placadevideo: "RTX 4090",
+    processador: "INTEL I9",
+    monitor: "AMOLED ASUS",
+    fonte: "FONTE 1050w",
+    memoriaram: "DDR5 4x16g RAM"
+}
+
+console.log(computador.placadevideo)
+
+var arraycomputador = Object.keys(computador)
+//INDEXOF: retorna se um termo existe o seu indice, caso não exista, -1
+console.log(arraycomputador.indexOf("fonte"))
+//INCLUDES: retorna se um termo existe, APENAS TRUE OU FALSE
+console.log(arraycomputador.includes("monitor"))
+
+
+
+//var pergunta = prompt("pesquisa de componentes para pc")
+var processo = arraycomputador.indexOf(pergunta)
+if(processo == -1){
+    //alert("Componente não existe")
+}else{
+    //alert("Componente existe")
+}
+
+console.log(Object.entries(computador))
+
